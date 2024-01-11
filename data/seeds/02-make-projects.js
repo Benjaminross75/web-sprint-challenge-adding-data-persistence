b6c5aebc6b1cd02f0042519006322a6f9a747e75
Ben Ross <mrbenross@gmail.com>
2024-01-11 16:38:02 -0600

    seed-file-created

--- data/seeds/02-make-projects.js
+++ data/seeds/02-make-projects.js
@@ -1,3 +1,71 @@
-exports.seed = function (knex){
-  
+const projects = [
+    {project_name: 'online task manager',
+    product_description: 'build task management app',
+    project_completed: false},
+
+    {project_name: 'expense tracker app',
+    product_description: 'develop mobile app for tracking expenses',
+     project_completed: true},
+
+    {project_name: 'contribute to open source project',
+    product_description: 'make contributions in open source projects',
+      project_completed: true},
+]
+
+const resources = [
+
+    {resource_name: 'javascript',
+     resource_description: 'script language for web development'},
+
+     {resource_name: 'postgreSQL',
+     resource_description: 'open source RDBS'},
+
+     {resource_name: 'Git',
+     resource_description: 'version control system'},
+
+]
+
+const tasks = [
+    {task_description: 'implement user authentication',
+     task_notes: 'utilize secure auth methods',
+     task_completed: true,
+     project_id: 1,
+     },
+
+     {task_description: 'design expense entry database',
+     task_notes: 'create user friendly interface',
+     task_completed: false,
+     project_id: 2,
+     },
+
+     {task_description: 'fix bugs',
+     task_notes: 'fix bugs in open source project',
+     task_completed: false,
+     project_id: 3,
+     },
+
+   ]
+
+   const project_resources = [
+    {project_id: 1, resource_id: 1},
+    {project_id: 1, resource_id: 2},
+    {project_id: 1, resource_id: 3},
+    {project_id: 2, resource_id: 1},
+    {project_id: 2, resource_id: 2},
+    {project_id: 2, resource_id: 3},
+    {project_id: 3, resource_id: 1},
+    {project_id: 3, resource_id: 3},
+   ]
+
+
+
+
+
+
+
+exports.seed = async function (knex){
+   await knex('projects').insert(projects)
+   await knex('resources').insert(resources)
+   await knex('tasks').insert(tasks)
+   await knex('project_resources').insert(project_resources)
 }
